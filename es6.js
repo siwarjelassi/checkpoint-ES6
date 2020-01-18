d1d4596dfe15323693cdb44d89160478fb7e62e2
siwarjelassi <56412397+siwarjelassi@users.noreply.github.com>
2020-01-18 14:55:59 +0100

    Add files via upload

---
+++ es6.js
@@ -0,0 +1,29 @@
+const pets = [
+  { name: "Max", type: "dog", bornOn: 2018 },
+  { name: "Angel", type: "cat", bornOn: 2015 },
+  { name: "Jasper", type: "dog", bornOn: 2016 }
+];
+
+const getAge = pet =>{ new date().getFullYear()- pet.bornOn}
+
+const petsWithAge = pets.map((pet)=>{
+  pet.age = getAge(pet);
+  petsWithAge.push(pet);
+})
+console.log(petsWithAge);
+
+const dogs = pets.filter(function(pet){
+  if (pet.type === "dog") {
+    dogs.push(pet);
+  }
+});
+console.log(dogs);
+
+
+const jasper=pets.find(function(pet){
+  if (pet.name === "Jasper") {
+    jasper = pet;
+  }
+});
+
+console.log(`Jasper is  + ${jasper.age}  +  years old `);
